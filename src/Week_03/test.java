d7d7b488c62683a28d9ea03bf73a1ecc811b2086
Thiloshon Nagarajah <thiloshon@gmail.com>
2016-07-16 19:14:34 +0530

    Week 03

---
+++ src/Week_03/test.java
@@ -0,0 +1,17 @@
+package Week_03;
+
+/**
+ * Created by Thiloshon on 16-Jul-16.
+ */
+public class test {
+
+    public static void main(String[] args) {
+        int count=0;
+        for (int x=0; x<100000; x++){
+            count++;
+            System.out.println(count);
+        }
+
+    }
+
+}
