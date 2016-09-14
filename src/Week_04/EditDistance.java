3ee7a5314cecb4473b7428b992996758ce2b1a1b
Thiloshon Nagarajah <thiloshon@gmail.com>
2016-09-14 11:36:54 +0530

    Week 04

---
+++ src/Week_04/EditDistance.java
@@ -0,0 +1,59 @@
+package Week_04;
+
+/**
+ * Created by Thiloshon on 12-Sep-16.
+ */
+import java.util.*;
+
+class EditDistance {
+    public static int EditDistance(String s, String t) {
+        //write your code here
+        char[] charArray = s.toCharArray();
+        char[] arr1 = new char[charArray.length+1];
+        //System.out.println(arr1.length);
+        System.arraycopy(charArray, 0, arr1, 1, charArray.length);
+        char[] charArray1 = t.toCharArray();
+        char[] arr2 = new char[charArray1.length+1];
+        //System.out.println(arr2.length);
+        System.arraycopy(charArray1, 0, arr2, 1, charArray1.length);
+
+        int[][] DArray = new int[charArray1.length+1][charArray.length+1];
+
+        for(int h =0; h<=charArray1.length;h++){
+           DArray[h][0]=h;
+        }
+        for(int g =0; g<=charArray.length;g++){
+            DArray[0][g]=g;
+        }
+
+        for(int i =1; i<arr2.length; i++){
+            for(int j = 1; j<arr1.length;j++){
+                int insertion = DArray[i][j-1]+1;
+                int deletion = DArray[i-1][j]+1;
+                int match = DArray[i-1][j-1];
+                int mismatch = DArray[i-1][j-1]+1;
+                //System.out.println("i : j = " +i+" : " + j);
+                if(arr1[j]==arr2[i]){
+
+                    int min1= Math.min(insertion, deletion);
+                    DArray[i][j]=Math.min(min1, match);
+                }else{
+                    int min1= Math.min(insertion, deletion);
+                    DArray[i][j]=Math.min(min1, mismatch);
+                }
+            }
+
+        }
+        return DArray[arr2.length-1][arr1.length-1];
+    }
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+
+        String s = scan.next();
+        String t = scan.next();
+
+        System.out.println(EditDistance(s, t));
+    }
+
+}
+
---
+++ src/Week_04/stack.java
@@ -0,0 +1,69 @@
+package Week_04;
+
+import java.util.Scanner;
+
+/**
+ * Created by Thiloshon on 13-Sep-16.
+ */
+public class stack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String exp = scanner.next();
+        parenthesis(exp);
+    }
+
+    private static void parenthesis(String expression) {
+        resultStorage = new long[100][100][100];
+        long[] results = parenthesis(expression, 0, 0);
+        System.out.println(results[1]);
+    }
+
+    private static long[][][] resultStorage;
+
+    public static long[] parenthesis(String expression, int start, int end) {
+        if (resultStorage[start][end][2] == 1)
+            return resultStorage[start][end];
+
+        try {
+            long parsedLong = Long.parseLong(expression);
+            return new long[] { parsedLong, parsedLong, 1 };
+        } catch (NumberFormatException e) {
+            //
+        }
+
+        long[] result = { Integer.MAX_VALUE, Integer.MIN_VALUE, 1 };
+        for (int i = 1; i < expression.length() - 1; i++) {
+            if (Character.isDigit(expression.charAt(i)))
+                continue;
+            long[] left = parenthesis(expression.substring(0, i), start, start + i);
+            long[] right = parenthesis(expression.substring(i + 1, expression.length()), start + i + 1, end);
+            for (int li = 0; li < 2; li++) {
+                for (int ri = 0; ri < 2; ri++) {
+                    switch (expression.charAt(i)) {
+                        case '+':
+                            result[0] = Math.min(result[0], left[li] + right[ri]);
+                            result[1] = Math.max(result[1], left[li] + right[ri]);
+                            break;
+                        case '-':
+                            result[0] = Math.min(result[0], left[li] - right[ri]);
+                            result[1] = Math.max(result[1], left[li] - right[ri]);
+                            break;
+                        case '*':
+                            result[0] = Math.min(result[0], left[li] * right[ri]);
+                            result[1] = Math.max(result[1], left[li] * right[ri]);
+                            break;
+                        case '/':
+                            if (right[ri] != 0)
+                                result[0] = Math.min(result[0], left[li] / right[ri]);
+                            if (right[ri] != 0)
+                                result[1] = Math.max(result[1], left[li] / right[ri]);
+                            break;
+                    }
+                }
+            }
+        }
+
+        resultStorage[start][end] = result;
+        return result;
+    }
+}
